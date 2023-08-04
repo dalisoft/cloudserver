ca110e97bef437757d497a512d637a886e46aced
Taylor McKinnon <mail@tmacs.space>
2023-08-04 09:38:10 -0700

    imprv test infra

--- tests/functional/raw-node/test/routes/routeBackbeat.js
+++ tests/functional/raw-node/test/routes/routeBackbeat.js
@@ -156,7 +156,8 @@ describeSkipIfAWS('backbeat routes', () => {
         bucketUtil = new BucketUtility(
             'default', { signatureVersion: 'v4' });
         s3 = bucketUtil.s3;
-        s3.createBucket({ Bucket: TEST_BUCKET }).promise()
+        bucketUtil.emptyManyIfExists([TEST_BUCKET, TEST_ENCRYPTED_BUCKET, NONVERSIONED_BUCKET])
+            .then(() => s3.createBucket({ Bucket: TEST_BUCKET }).promise())
             .then(() => s3.putBucketVersioning(
                 {
                     Bucket: TEST_BUCKET,
@@ -197,7 +198,7 @@ describeSkipIfAWS('backbeat routes', () => {
             .then(() => s3.deleteBucket({ Bucket: TEST_ENCRYPTED_BUCKET }).promise())
             .then(() =>
                 s3.deleteBucket({ Bucket: NONVERSIONED_BUCKET }).promise())
-            .then(() => done());
+            .then(() => done(), err => done(err));
     });
 
     describe('null version', () => {
@@ -219,12 +220,16 @@ describeSkipIfAWS('backbeat routes', () => {
             assert.strictEqual(StorageClass, 'STANDARD');
         }
 
-        beforeEach(done => s3.createBucket({ Bucket: BUCKET_FOR_NULL_VERSION }, done));
-        afterEach(done => {
+        beforeEach(done =>
+            bucketUtil.emptyIfExists(BUCKET_FOR_NULL_VERSION)
+                .then(() => s3.createBucket({ Bucket: BUCKET_FOR_NULL_VERSION }).promise())
+                .then(() => done(), err => done(err))
+        );
+        afterEach(done =>
             bucketUtil.empty(BUCKET_FOR_NULL_VERSION)
                 .then(() => s3.deleteBucket({ Bucket: BUCKET_FOR_NULL_VERSION }).promise())
-                .then(() => done());
-        });
+                .then(() => done(), err => done(err))
+        );
 
         it('should update metadata of a current null version', done => {
             let objMD;
